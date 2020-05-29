798a74124e5195de3d33cd2eee65a54a68b79500
Vagif Ibrahimov <vaqa.ibrahim@gmail.com>
2020-05-29 23:20:40 +0400

    JavascriptSliderProjekt

--- script.js
+++ script.js
@@ -32,8 +32,49 @@ function slaydShow(SlaytNumber) { // Şəkillər bu funksiyaya əsasən dəyişi
     }
 
 
-    for (i = 0; i < slaydSay; i++) { // Şəkillər bu funksiyaya əsasən görünür
+    for (i = 0; i < slaydSay; i++) { // Şəkillər bu funksiyaya əsasən gizlədilir
         slayd[i].style.display = "none";
     }
-    slayd[slaydNo].style.display = "block";
+    slayd[slaydNo].style.display = "block"; // Şəkillər bu hissəyə görə tək tək görünür
 }
+
+// Drag and Drop 1 Start
+
+var element = document.querySelector('#a');
+
+element.addEventListener("mousedown", start); //MouseDown(Tut) + MouseUp(buraxmaq)=Click
+
+function start() { //mousemove - boxu hərəkətə gətrir
+    addEventListener("mousemove", move);
+}
+
+function move(e) {
+    e = e || event;
+
+    element.style.left = e.pageX - 50 + "px"; //box-un hərəkətə gətirilməsi üçün tutulduğu yer
+    element.style.top = e.pageY - 50 + "px";
+}
+
+element.addEventListener("mouseup", function () {
+    removeEventListener("mousemove", move);
+})
+
+// Drag and Drop 1 End
+
+// Drag and Drop 2 Start  Bunu tam başa düşmədim 
+function allowDrop(home) {
+    home.preventDefault();
+}
+
+function drag(home) {
+    home.dataTransfer.setData("text", home.target.id);
+}
+
+function drop(home) {
+    home.preventDefault();
+    var data = home.dataTransfer.getData("text");
+    home.target.appendChild(document.getElementById(data));
+}
+
+
+// Drag and Drop 2 End
